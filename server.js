5b6bbd857099a450e94ae919c1d9628c4dc440aa
JoKing <joebryant614@gmail.com>
2021-05-19 11:38:05 -0700

    routes added for zoo

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function filterByQuery(query, animalsArray) {
             );
         });
     }
-    
+
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -41,7 +41,12 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -50,6 +55,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
